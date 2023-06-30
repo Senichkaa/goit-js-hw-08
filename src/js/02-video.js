7fdfa90d79da58128cc77387f2f63defb279313a
Senichkaa <super-kapanaiko406@ukr.net>
2023-06-30 16:20:39 +0300

    2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player
+  .setCurrentTime(localStorage['videoplayer-current-time'])
+  .then(timeUpdate)
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });

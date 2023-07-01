0a60d7a743e61959a38095c554897a61f77f39a9
Senichkaa <super-kapanaiko406@ukr.net>
2023-07-01 18:15:39 +0300

    fixed 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,17 @@ const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(timeUpdate, 1000));
 
 function timeUpdate(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(currentTime, data.seconds);
 }
 
 player
-  .setCurrentTime(localStorage['videoplayer-current-time'])
-  .then(data.seconds)
+  .setCurrentTime(localStorage.getItem(currentTime))
+  .then(player.play())
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
